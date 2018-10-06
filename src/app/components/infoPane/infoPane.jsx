e6717b9442f048c057d05229482a8cf411b67e1b
RaptorCentauri <dave.leo.shilander@icloud.com>
2018-10-05 20:16:21 -0500

    backup

--- src/app/components/infoPane/infoPane.jsx
+++ src/app/components/infoPane/infoPane.jsx
@@ -3,12 +3,22 @@ import './InfoPane.scss';
 
 const InfoPane = (props) =>
     <div id='info-pane'>
-        <h5>Name: {props.name}</h5>
-        <h5>Plot: {props.plot}</h5>
-        <h5>Rating: {props.rated}</h5>
-        <h5>Runtime: {props.runtime}</h5>
-        <h5>Director: {props.director}</h5>
-        <h5>Production Company: {props.production}</h5>
+        <div id='info-top'>
+                <img src={props.poster} alt="" />
+            <div id='info-top-text'>
+                <h2>Name: {props.name}</h2>
+                <h2>Director: {props.director}</h2>
+            </div>
+
+        </div>
+        <div id='info-bottom'>
+            <h2>Plot: {props.plot}</h2>
+            <h2>Rating: {props.rated}</h2>
+            <h2>Runtime: {props.runtime}</h2>
+            <h2>Release Date: {props.released}</h2>
+            <h2>Production Company: {props.production}</h2>
+        </div>
+
     </div>
 
 export default InfoPane
--- src/app/index.js
+++ src/app/index.js
@@ -30,26 +30,36 @@ class App extends React.Component{
   handeResultClick = async (id) => {
       let result = await getById(id);
       this.setState({info: result});
+      console.log(this.state.info);
+      
   }
+
 
     render(){
         return(
             <div className='App'>
-                <Search handleInputChange={this.handleSearchInputChange} handleClick={this.handleSearchClick}/>
-                <div id='search-results'>
-                {this.state.searchResults.map(i => <Results  key={i.imdbid} title={i.title} type={i.type.charAt(0).toUpperCase()+i.type.slice(1)} year={i.year} poster={i.poster} handleClick={this.handeResultClick.bind(this, i.imdbid)}/>)}
+                <div id='left-box'>
+                    <Search handleInputChange={this.handleSearchInputChange} handleClick={this.handleSearchClick}/>
+                    <div id='result-box'>
+                        {this.state.searchResults.map(i => <Results  key={i.imdbid} title={i.title} type={i.type.charAt(0).toUpperCase()+i.type.slice(1)} year={i.year} poster={i.poster} handleClick={this.handeResultClick.bind(this, i.imdbid)}/>)}
+                    </div>
                 </div>
+                <div id='right-box'>
                 {/* {this.state.info ?  */}
                 <InfoPane 
+                    poster={this.state.info.poster}
                     name={this.state.info.name}
                     plot={this.state.info.plot}
                     rated={this.state.info.rated}
                     runtime={this.state.info.runtime}
                     director={this.state.info.director}
                     production={this.state.info.production}
+                    released={String(this.state.info.released)}
                 />
                 {/* : false */}
                 {/* } */}
+                </div>
+
             </div>
         );
     }
